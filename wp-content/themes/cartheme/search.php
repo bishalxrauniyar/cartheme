c9c485b0b8324d7f80d80a767270fdcc2fe5d7cc
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-02-07 14:54:47 +0545

    search page working now

--- wp-content/themes/cartheme/search.php
+++ wp-content/themes/cartheme/search.php
@@ -6,7 +6,16 @@ get_header();
 <br>
 <br>
 <br>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br>
 <?php include 'searchform.php'; ?>
+
+<br>
 <?php
 // Get search parameters from the GET request
 $car_year = isset($_GET['car_year']) ? sanitize_text_field($_GET['car_year']) : '';
@@ -80,67 +89,66 @@ $search_query = new WP_Query($args);
 <?php
 if ($search_query->have_posts()) :
     while ($search_query->have_posts()) : $search_query->the_post();
+
+        $car_model = get_the_terms(get_the_ID(), 'car_model');
+        $car_year = get_the_terms(get_the_ID(), 'car_year');
+        $car_price = get_post_meta(get_the_ID(), 'car_price', true);
+        $car_mileage = get_post_meta(get_the_ID(), 'car_mileage', true);
+        $car_hp = get_post_meta(get_the_ID(), 'car_hp', true);
+        $car_transmission = get_the_terms(get_the_ID(), 'car_transmission');
+
 ?>
         <div class="container">
             <div class="single-cars-content">
-                <?php
-                if (have_posts()) :
-                    while (have_posts()) : the_post();
-                        $car_model = get_the_terms(get_the_ID(), 'car_model');
-                        $car_year = get_the_terms(get_the_ID(), 'car_year');
-                        $car_price = get_post_meta(get_the_ID(), 'car_price', true);
-                        $car_mileage = get_post_meta(get_the_ID(), 'car_mileage', true);
-                        $car_hp = get_post_meta(get_the_ID(), 'car_hp', true);
-                        $car_transmission = get_the_terms(get_the_ID(), 'car_transmission');
-                ?>
-                        <div class="single-cars-item">
-                            <div class="single-single-cars-item">
-                                <div class="row align-items-center"> <!-- Center content -->
-                                    <div class="col-md-6 col-sm-12 text-center">
-                                        <div class="single-cars-img">
-                                            <img src="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"
-                                                alt="<?php echo esc_attr(get_the_title()); ?>" class="img-fluid rounded">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 col-sm-12">
-                                        <div class="single-cars-txt">
-                                            <h2 class="car-title">
-                                                <?php the_title(); ?>
-                                                <span class="car-model">
-                                                    <?php echo ($car_model && !is_wp_error($car_model)) ? esc_html($car_model[0]->name) : 'N/A'; ?>
-                                                </span>
-                                                <span class="car-price">
-                                                    <?php echo (!empty($car_price)) ? '$' . esc_html($car_price) : ''; ?>
-                                                </span>
-                                            </h2>
-                                            <p><?php the_content(); ?></p>
+                <div class="single-cars-item">
+                    <div class="single-single-cars-item">
+                        <div class="row align-items-center"> <!-- Center content -->
+                            <div class="col-md-6 col-sm-12 text-center">
+                                <div class="single-cars-img">
+                                    <img src="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"
+                                        alt="<?php echo esc_attr(get_the_title()); ?>" class="img-fluid rounded">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <div class="single-cars-txt">
+                                    <h2 class="car-title">
+                                        <?php the_title(); ?>
+                                        <span class="car-model">
+                                            <?php echo ($car_model && !is_wp_error($car_model)) ? esc_html($car_model[0]->name) : 'N/A'; ?>
+                                        </span>
+                                        <span class="car-price">
+                                            <?php echo (!empty($car_price)) ? '$' . esc_html($car_price) : ''; ?>
+                                        </span>
+                                    </h2>
+                                    <p><?php the_content(); ?></p>
 
-                                            <div class="featured-model-info">
-                                                <p>
-                                                    <strong>Model:</strong> <?php echo ($car_year && !is_wp_error($car_year)) ? esc_html($car_year[0]->name) : 'N/A'; ?>
-                                                    <span class="featured-mi-span">
-                                                        <strong>Mileage:</strong> <?php echo (!empty($car_mileage)) ? esc_html($car_mileage) . ' miles' : ''; ?>
-                                                    </span>
-                                                    <span class="featured-hp-span">
-                                                        <strong>Horsepower:</strong> <?php echo (!empty($car_hp)) ? esc_html($car_hp) . ' HP' : ''; ?>
-                                                    </span>
-                                                    <strong>Transmission:</strong> <?php echo ($car_transmission && !is_wp_error($car_transmission)) ? esc_html($car_transmission[0]->name) : 'N/A'; ?>
-                                                </p>
-                                            </div>
-                                        </div>
+                                    <div class="featured-model-info">
+                                        <p>
+                                            <strong>Model:</strong> <?php echo ($car_year && !is_wp_error($car_year)) ? esc_html($car_year[0]->name) : 'N/A'; ?>
+                                            <span class="featured-mi-span">
+                                                <strong>Mileage:</strong> <?php echo (!empty($car_mileage)) ? esc_html($car_mileage) . ' miles' : ''; ?>
+                                            </span>
+                                            <span class="featured-hp-span">
+                                                <strong>Horsepower:</strong> <?php echo (!empty($car_hp)) ? esc_html($car_hp) . ' HP' : ''; ?>
+                                            </span>
+                                            <strong>Transmission:</strong> <?php echo ($car_transmission && !is_wp_error($car_transmission)) ? esc_html($car_transmission[0]->name) : 'N/A'; ?>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                <?php
-                    endwhile;
-                endif;
-                ?>
+                    </div>
+                </div>
             </div>
+        </div>
         </div>
 <?php
     endwhile;
+// No results found
+else :
+    echo '<div class="noresult"><h1>No cars found</h1></div>';
 endif;
+
 ?>
 <?php
 get_footer();
